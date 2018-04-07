6e34bf1e214f9204f68bf626196ee1c860f3c3ba
OisCircle <793414670@qq.com>
2018-04-07 13:25:42 +0800

    new features developing 3

--- wifi_monitor/src/main/java/com/qcq/wifi_monitor/controller/MinuteController.java
+++ wifi_monitor/src/main/java/com/qcq/wifi_monitor/controller/MinuteController.java
@@ -1,6 +1,7 @@
 package com.qcq.wifi_monitor.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qcq.wifi_monitor.vo.Minute;
@@ -13,6 +14,7 @@ public class MinuteController {
 		System.out.println("成功设置全局分钟:"+minute);
 	}
 	@RequestMapping(value="/getMinute")
+	@ResponseBody
 	public int getMinute(){
 		return this.minute.getMinute();
 	}
